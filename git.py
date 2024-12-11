f529e78607c015bc210326564f19f6d421151ec8
MAKH-DANIEL <danil3shalo@gmail.com>
2024-12-11 18:35:28 +0300

    добавил работу

--- git.py
+++ git.py
@@ -4,3 +4,11 @@ print(a)
 f = False
 p = print
 p('aasdafasf')
+
+my_strings = input('Введите имя: ')
+print(len(my_strings))
+print('здравствуйте', my_strings.upper())
+print('здравствуйте', my_strings.lower())
+print('Введите имя:'.replace(' ', ''))
+print(my_strings[0])
+print(my_strings[-1])
